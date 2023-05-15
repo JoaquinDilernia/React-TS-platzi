06cdc12cfa60c88810e47560e254fb2c7b3a4278
joacodiler <jdilernia99@gmail.com>
2023-05-15 15:13:33 -0300

    useStates

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,15 +1,14 @@
-// Generates a random number between 1 and 122
-const randomNumber = () => Math.floor(Math.random() * 122) + 1;
-
-export function RandomFox(): JSX.Element {
-const image = `https://randomfox.ca/images/${randomNumber()}.jpg`;
-
-return (
-    <img
-    width="320"
-    height="auto"
-    src={image}
-    className="mx-auto rounded-md bg-gray-300"
-    />
-);
-}
+type Props = {
+    image: string;
+  };
+  
+  export function RandomFox({ image }: Props): JSX.Element {
+    return (
+      <img
+        width="320"
+        height="auto"
+        src={image}
+        className="mx-auto rounded-md bg-gray-300"
+      />
+    );
+  }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { RandomFox } from "../components/RandomFox";
 
+// random number from 1 to 122
+const random = () => Math.floor(Math.random() * 122) + 1;
+
 const Home: NextPage = () => {
+  const [images, setImages] = useState<Array<string>>([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+  ]);
+
   return (
     <div>
       <Head>
@@ -12,7 +22,11 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <RandomFox />;
+        {images.map((image, index) => (
+          <div className="p-4" key={index}>
+            <RandomFox image={image} />
+          </div>
+        ))}
       </main>
 
       <footer></footer>
